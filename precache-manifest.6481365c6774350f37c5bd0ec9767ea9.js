b0ead180d33b6c7c1b77767111073632226deda4
Frank <frankyu1999@gmail.com>
2020-05-21 00:49:52 -0400

    Updates

---
+++ precache-manifest.6481365c6774350f37c5bd0ec9767ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642f42e8a3f3ca19872fb1447a2eefb8",
+    "url": "/Tierlist/index.html"
+  },
+  {
+    "revision": "c513e54fec960da5b4e5",
+    "url": "/Tierlist/static/css/main.45ea22dc.chunk.css"
+  },
+  {
+    "revision": "503b65c62fccff4b3374",
+    "url": "/Tierlist/static/js/2.768cf1c3.chunk.js"
+  },
+  {
+    "revision": "61cfc55582e6854b5e904d0a450e50e3",
+    "url": "/Tierlist/static/js/2.768cf1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c513e54fec960da5b4e5",
+    "url": "/Tierlist/static/js/main.b0ec9891.chunk.js"
+  },
+  {
+    "revision": "fda989f93f8bf9913fc9",
+    "url": "/Tierlist/static/js/runtime-main.079a816c.js"
+  }
+]);
