d66e6a42c33cd7b7fd69e1ec5e2e7b0f0b504ca8
doodlehead <frankyu1999@gmail.com>
2025-03-02 04:41:50 -0500

    remove firebase

--- src/components/Firebase/context.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Firebase from "./firebase";
-
-const FirebaseContext = React.createContext<Firebase | null>(null);
-
-export default FirebaseContext;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.tsx";
 import * as serviceWorker from "./serviceWorker";
-import Firebase, { FirebaseContext } from "./components/Firebase";
+// import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={new Firebase()}>
+    {/* <FirebaseContext.Provider value={new Firebase()}> */}
       <App />
-    </FirebaseContext.Provider>
+    {/* </FirebaseContext.Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
